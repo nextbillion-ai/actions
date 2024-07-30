b851460b70eb80d83dff480a543c2a951ee37e3d
chang <chang@nextbillion.ai>
2024-07-30 11:34:03 +0800

    fix: typo

--- git-envs/run.sh
+++ git-envs/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 echo "CI_COMMIT_SHA=$GITHUB_SHA" >> $GITHUB_ENV
-echo "CI_COMMIT_AUTHOR=$GITHUB_PUSHER_NAME >> $GITHUB_ENV
+echo "CI_COMMIT_AUTHOR=$GITHUB_PUSHER_NAME" >> $GITHUB_ENV
 if [[ "$GITHUB_REF" == refs/tags/* ]]; then
     echo "CI_COMMIT_TAG=${GITHUB_REF#refs/tags/}" >> $GITHUB_ENV
 fi
