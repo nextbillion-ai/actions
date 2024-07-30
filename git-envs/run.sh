98680968219de1962d992377fee62dcaf04d35c8
chang <chang@nextbillion.ai>
2024-07-30 11:39:34 +0800

    fix: add debug log

--- git-envs/run.sh
+++ git-envs/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo $GITHUB_PUSHER_NAME, $GITHUB_SHA, $GITHUB_REF
+
 echo "CI_COMMIT_SHA=$GITHUB_SHA" >> $GITHUB_ENV
 echo "CI_COMMIT_AUTHOR=$GITHUB_PUSHER_NAME" >> $GITHUB_ENV
 if [[ "$GITHUB_REF" == refs/tags/* ]]; then
